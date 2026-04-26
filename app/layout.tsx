9bea5d97adc6f034b4b093df0235dd0470dc1262
—4ᴇᴛʜᴇʀᴠᴇɪʟ <gamearz64@gmail.com>
2026-04-26 20:39:28 +0700

    update app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css'; // Global styles
+import './globals.css';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -12,10 +12,16 @@ export const metadata: Metadata = {
   description: 'Convert your website into a native Android and iOS application easily. Developed by SANN404 FORUM.',
 };
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="id" className={`${inter.variable}`}>
-      <body className="antialiased font-sans" suppressHydrationWarning>{children}</body>
+      <body className="antialiased font-sans" suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
